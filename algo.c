760278476e33549a716685f350e6e02ec49c8910
Anurag Arwalkar <anurag.arwalkar@rockwellautomation.com>
2022-08-17 21:50:31 +0530

    removed

--- algo.c
+++ algo.c
@@ -3,11 +3,12 @@
 
 /* Function to print an array */
 EMSCRIPTEN_KEEPALIVE
-void printArray(int arr[], int size)
+int printArray(int arr[], int size)
 {
     int i;
     for (i=0; i < size; i++)
         printf("%d ", arr[i]);
     printf("\n");
+    return 1;
 }
  
